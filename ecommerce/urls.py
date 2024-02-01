4c3417aca34baedbb1968082a25caad35f859f9d
netosarmento <netosarmento@gmail.com>
2024-02-01 14:25:42 -0300

    devevelop-search

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -38,6 +38,7 @@ urlpatterns = [
     path('register/', register_page, name='register'),
     path('bootstrap/', TemplateView.as_view(template_name='bootstrap/example.html')),
     path('products/', include("products.urls", namespace="products")),
+    path('search/', include("search.urls", namespace="search")),
     path('admin/', admin.site.urls),
 ]
 
